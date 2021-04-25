c4286afdcec9dac3ccc4a58de114196236fff354
yongjuer <yongjuer@gmail.com>
2021-04-26 01:16:29 +0800

    second commit

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>
-      <!-- 子路由出口 -->
-      <router-view></router-view>
-  </div>
+  <el-container>
+    <el-aside width="200px">
+    <!-- 侧边栏组件 -->
+    <app-aside></app-aside>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <!-- 头部组件 -->
+        <app-header></app-header>
+      </el-header>
+      <el-main>
+        <!-- 设置子路由出口 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+// 引入侧边栏组件
+import AppAside from './components/AppAside'
+// 引入头部组件
+import AppHeader from './components/AppHeader'
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    AppAside,
+    AppHeader
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-container {
+  height: 100vh;
+  // min-width: 980px;
+}
+
+.el-aside {
+  background-color: #d3dce6 !important;
+}
+
+.el-header {
+  background-color: #fff;
+}
+
+.el-aside {
+  background-color: #e9eef3;
+}
+</style>
